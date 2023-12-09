2feed65a42b420539a1aa458cfb6eddfe04a91ee
Mahmoud174200 <afmahmoudahmed@gmail.com>
2023-12-09 11:43:42 +0200

    creat add,delete and edit products

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,9 +1,38 @@
-import React from "react";
 import '../css/DashBoardAdmin.css';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-const AddProduct=()=>{
-    return(
-        <div className="BackGround">
+import axios from "axios";
+
+const AddProduct = () => {
+  const [name, setProductName] = useState("");
+  const [price, setProductPrice] = useState("");
+  const [image, setUrl] = useState("");
+  const [productAdded, setProductAdded] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/v1/products', {
+        name: name,
+        price: price,
+        image: image,
+      });
+
+      console.log("Product added successfully:", response.data);
+
+      setProductAdded(true);
+
+    } catch (error) {
+        setProductAdded(false);
+
+      console.error("Error while adding the product:", error);
+    }
+  };
+
+  return (
+    <div className="BackGround">
+        
         <aside className="sideBar">
         <div
             className="link">
@@ -23,9 +52,52 @@ const AddProduct=()=>{
             <div className="link">
             <Link className="Links" to={'editProduct'}>Delete Item Product</Link>
             </div>
+             {/* {setProductAdded && (
+          <p style={{ color: 'white' }}>تمت إضافة المنتج بنجاح!</p>
+        )} */}
         </aside>
-        </div>
-    )
-}
+        <div>
+
+        <form onSubmit={handleSubmit}>
+       
+          <label htmlFor="product_name">Product Name:</label>
+          <input
+            type="text"
+            id="product_name"
+            name="product_name"
+            value={name}
+            onChange={(e) => setProductName(e.target.value)}
+            required
+          />
+
+          <label htmlFor="product_price">Product Price:</label>
+          <input
+            type="text"
+            id="product_price"
+            name="product_price"
+            value={price}
+            onChange={(e) => setProductPrice(e.target.value)}
+            required
+          />
+          <label htmlFor="product_price">Product url:</label>
+          <input
+            type="text"
+            id="product_price"
+            name="product_price"
+            value={image}
+            onChange={(e) => setUrl(e.target.value)}
+            required
+          />
+
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+      
+    </div>
+    
+
+
+  );
+};
 
 export default AddProduct;
--- src/pages/DeleteProduct.js
+++ src/pages/DeleteProduct.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../css/DashBoardAdmin.css';
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 const DeleteProduct=()=>{
+    const [id, setId] = useState("");
+
+  const handleDelete = async () => {
+    try {
+      // Make a DELETE request to your API endpoint with the specified product ID
+      await axios.delete(`/api/v1/products/${id}`);
+      console.log("Product deleted successfully!");
+
+      // You can add redirection to another page or perform additional actions here
+    } catch (error) {
+      console.error("Error while deleting the product:", error);
+    }
+  };
     return(
         <div className="BackGround">
         <aside className="sideBar">
+        <form onSubmit={handleDelete}>
+
+        <label htmlFor="product_id">Product ID:</label>
+        <input
+          type="text"
+          id="product_id"
+          name="product_id"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          required
+        />
+                <input type="submit" value="Delete" />
+
+        </form>
         <div
             className="link">
             <Link className="Links" to={'/'}>DashBoard</Link>
